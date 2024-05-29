3cf5be28bf4edb9afbf04b5f39483b5409b0b9f4
Jayant Anand <jayantanand2001@gmail.com>
2024-05-29 23:08:51 +0530

    Modified constants

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include <csignal>
 #include "VL53L0X.hpp"
 
-#define LEFT_ANGLE 45
-#define RIGHT_ANGLE 135
+#define LEFT_ANGLE 60
+#define RIGHT_ANGLE 130
 #define SERVO_PIN RPI_GPIO_P1_12  // GPIO 18
 
 VL53L0X sensor;  // Device instance for the VL53L0X
@@ -35,9 +35,9 @@ void set_servo_angle(int angle) {
 void* rotate_servo(void* arg) {
     while (!exitFlag) {
         set_servo_angle(LEFT_ANGLE);
-        usleep(50000);  // 50ms delay
+        usleep(39000);  // 81ms delay
         set_servo_angle(RIGHT_ANGLE);
-        usleep(50000);  // 50ms delay
+        usleep(39000);  // 50ms delay
     }
     return NULL;
 }
@@ -57,7 +57,6 @@ void* read_lidar(void* arg) {
         } catch (const std::exception& error) {
             std::cerr << "Error getting measurement: " << error.what() << std::endl;
         }
-        usleep(100000);  // 100ms delay
     }
     return NULL;
 }
